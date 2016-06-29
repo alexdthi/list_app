5c7673a210b16bdd4e565b0224948375a528b576
Alex Tong <alexdthi@gmail.com>
2016-06-28 14:31:10 -1000

    listy
    listy

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
-var	colors = ]
-	"red"
-	"orange"
-	"yellow"
-	"greeen"
-	"blue"
-	"indigo"
-	"violet"
+var	colors = [
+	"red",        //0
+	"orange",     //1
+	"yellow",     //2
+	"green",     //3
+	"blue",       //4
+	"indigo",     //5
+	"violet",     //6
 	];
 
 var count = 0;
 
 var cycle =	function(array) {
-	console.log(array[count]);
+	// console.log(array[count]);
 	if (count == array.length-1) {
 		count = 0;
 	} else {
@@ -20,5 +20,17 @@ var cycle =	function(array) {
 };
 
 var display = function(){
+	document.body.style.backgroundColor = colors[count];
 	cycle(colors);
 };
+
+ var listAll = function(array) {
+ 	for (var i = 0; i < array.length; i++) {
+ 		document.getElementById('colorSpace').innerHTML +=
+ 		"<li>" + array[i] = "</li>"; // <li>green</li>
+ 	}
+ }
+
+
+
+listAll(colors);
